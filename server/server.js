5edb237477e8b3d7ce9e612cba551fd1604848e5
jacob knudsen <jhakeknudsen@protonmail.com>
2022-03-16 12:26:02 -0600

    trying to fix not found

--- server/server.js
+++ server/server.js
@@ -6,17 +6,20 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '../public'));
+})
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "../public../index.html"));
 })
 
 app.get("/styles", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/index.css"))
+    res.sendFile(path.join(__dirname, "../public/index.css"));
 })
 
 const port = process.env.PORT || 4005; 
 
 app.listen(port, () =>{
-    console.log(`its all good ${port}`)
+    console.log(`its all good ${port}`);
 })
